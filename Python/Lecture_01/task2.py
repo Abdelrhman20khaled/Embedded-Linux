1f922656aa55c75d879d6589079cde35de7683bc
Abdelrhman20khaled <abdelrahmankh2025@gmail.com>
2024-06-14 15:47:49 +0300

    Solve All Issues

--- Python/Lecture_01/task2.py
+++ Python/Lecture_01/task2.py
@@ -11,7 +11,7 @@ my_char = input("Enter the character: ")
 #Put the Vowel String that we should serach the input char in it 
 Vowel_L = "aieou"
 #if the input char in 'aieou' or in 'AIEOU' it will print that is Vowel
-if (my_char in Vowel_L) or (my_char in Vowel_L.capitalize()):
+if (my_char in Vowel_L) or (my_char in Vowel_L.upper()):
     print("Your Char is Vowel")
 else :
     print("Your Char is not Vowel")   
